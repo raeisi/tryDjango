c34a3b7e967141048fb22097f993f3b53a7f7651
raeisi <s.raeisi@yahoo.co.uk>
2015-08-31 21:34:31 +0200

    end of 14th Video

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-
 from .models import SignUp
 
 class SignUpForm(forms.ModelForm):
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
+
 # Create your views here.
 def home(request):
 	title = 'Welcome'
-	if request.user.is_authenticated():
-		title = 'Hello %s!!' %(request.user)
-
+	form = SignUpForm(request.POST or None)
+	
 	context = {
 		'title': title,
+		'form': form
 	}
+
+	if form.is_valid():
+		FORM.SAVE()
+		# instance = form.save(commit=False)
+
+		# full_name = form.cleaned_data.get('full_name')
+		# if not full_name:
+		# 	full_name = 'New Full Name'
+		# instance.full_name = full_name 
+
+		# if not instance.full_name:
+		# 		instance.full_name = 'Jasmine'
+		#instance.save()
+		context = {
+			"title" : 'Thank you'
+		}
+
+
 	return render(request, "home.html", context)
