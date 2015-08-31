8227159c172f8c3303271bce97e7cd6e6aefbe98
raeisi <s.raeisi@yahoo.co.uk>
2015-08-31 16:05:54 +0200

    Added SignUpAdmin Class

--- src/newsletter/models.py
+++ src/newsletter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+	email = models.EmailField(max_length=254,blank=False)
+	full_name = models.CharField(max_length=50, blank=False, null=False)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+	def __unicode__(self): #Python 3 __str__
+		return self.email
